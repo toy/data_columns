073c4de94f031c4693583e317c8cbc89c41da900
Boba Fat <ivan@workisfun.ru>
2009-04-16 18:13:16 +0400

    type, default, dirty states

--- lib/data_columns.rb
+++ lib/data_columns.rb
@@ -3,13 +3,44 @@ module DataColumns
     base.extend ClassMethods
   end
 
+  def read_data_column(name)
+    name = name.to_s
+    converter = self.class.data_column_converters[name]
+    (self[:data] || {})[name] || converter.default
+  end
+
+  def write_data_column(name, value)
+    name = name.to_s
+    converter = self.class.data_column_converters[name]
+
+    if changed_attributes.include?(name)
+      old = changed_attributes[name]
+      changed_attributes.delete(name) unless field_changed?(name, old, value)
+    else
+      old = clone_attribute_value(:read_data_column, name)
+      changed_attributes[name] = old if field_changed?(name, old, value)
+    end
+
+    self[name] = value
+
+    (self[:data] ||= {})[name] = converter.type_cast(value)
+  end
+
   module ClassMethods
+    def data_column_converters
+      read_inheritable_attribute(:data_column_converters) || write_inheritable_attribute(:data_column_converters, {})
+    end
+
     def data_column_names
-      read_inheritable_attribute(:data_column_names) || write_inheritable_attribute(:data_column_names, [])
+      data_column_converters.keys
     end
 
     def data_columns(*columns)
-      if data_column_names.empty?
+      options = columns.extract_options!
+      type = options[:type] && options[:type].to_s
+      default = options[:default]
+
+      if data_column_converters.empty?
         serialize :data, Hash
         attr_protected :data
         class_eval %Q{
@@ -20,16 +51,16 @@ module DataColumns
       end
 
       columns.each do |column|
-        data_column_names << column.to_s
+        column = column.to_s
+        data_column_converters[column] = ActiveRecord::ConnectionAdapters::Column.new(column, default, type)
         class_eval %Q{
           def #{column}
-            (self[:data] || {})[:#{column}]
+            read_data_column(:#{column})
           end
           def #{column}=(value)
-            (self[:data] ||= {})[:#{column}] = value
+            write_data_column(:#{column}, value)
           end
         }, __FILE__, __LINE__
-        # write_attribute(:#{column}, value)
       end
     end
   end
--- spec/data_columns_spec.rb
+++ spec/data_columns_spec.rb
@@ -3,10 +3,12 @@ require File.dirname(__FILE__) + '/spec_helper'
 class Person < ActiveRecord::Base
   data_columns :icq, :skype
   data_columns :phone
+
+  data_columns :x, :y, :z, :type => :integer
+  data_columns :a, :b, :type => :integer, :default => '123px'
 end
 
 class Worker < Person
-  attr_accessor :password
   data_columns :work_phone
 end
 
@@ -28,46 +30,48 @@ describe DataColumns do
     w.work_phone.should == '367892'
   end
 
+  it "should convert values for column with type" do
+    Person.create(:x => '12345px', :y => 'aaa')
+    p = Person.first
+    p.x.should == 12345
+    p.y.should == 0
+    p.z.should == nil
+  end
+
+  it "should convert and use default for column with type and default" do
+    Person.create(:a => nil)
+    p = Person.first
+    p.a.should == 123
+    p.b.should == 123
+  end
+
   it "should return all data_column_names for Person" do
-    Person.data_column_names.should == %w(icq skype phone)
+    Person.data_column_names.sort.should == %w(icq skype phone x y z a b).sort
   end
 
   it "should return all data_column_names for Worker" do
-    Worker.data_column_names.should == %w(icq skype phone work_phone)
+    Worker.data_column_names.sort.should == %w(icq skype phone work_phone x y z a b).sort
   end
 
   it "should return all column_names for Person" do
-    Person.column_names.should == %w(id type name data icq skype phone)
+    Person.column_names.sort.should == %w(id type name data icq skype phone x y z a b).sort
   end
 
   it "should return all column_names for Worker" do
-    Worker.column_names.should == %w(id type name data icq skype phone work_phone)
+    Worker.column_names.sort.should == %w(id type name data icq skype phone work_phone x y z a b).sort
   end
 
-  # it "should track dirty state" do
-  #   Person.create(:icq => '12345', :skype => 'vasa', :phone => '56789')
-  #   person = Person.first
-  #   person.icq = 'ast'
-  #   person.changed.should == ['icq']
-  #   person.password_will_change!
-  #   # person.skype.upcase!
-  #   # person.changed.should == ['icq']
-  # end
+  it "should track dirty state" do
+    Person.create(:icq => '12345', :skype => 'vasa', :phone => '56789')
 
-  # it "should track dirty state" do
-  #   Person.create(:icq => '12345', :skype => 'vasa', :phone => '56789')
-  #   person = Person.first
-  #   p person.icq
-  #   person.icq = ''
-  #   p person.icq_changed?
-  #   p person.icq_change
-  #   p person.icq_will_change!
-  #   p person.icq_was
-  #   # person.icq = '23456'
-  #   # person.changed.should == ['icq']
-  #   # person.password_will_change!
-  #   # person.skype.upcase!
-  #   # person.changed.should == ['icq']
-  # end
+    person = Person.first
+    person.icq = nil
+    person.changed.should == ['icq']
+    person.icq_change.should == ['12345', nil]
 
+    person = Person.first
+    person.icq = 'lala'
+    person.changed.should == ['icq']
+    person.icq_change.should == ['12345', 'lala']
+  end
 end
